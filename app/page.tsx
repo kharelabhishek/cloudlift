9914d37b757c5dc1a23eb09b9d8efa23e1e99a0d
kharelabhishek <kharela86@gmail.com>
2024-11-25 18:29:56 +0545

    feat: signin and signup page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex-center h-screen ">
+      <h1 className="text-3xl ">Cloudlift</h1>
     </div>
   )
 }
